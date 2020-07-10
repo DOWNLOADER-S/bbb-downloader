68c879342972f21085b8adfca94ad508221c2e83
A-d-r-i <charbadrien@hotmail.fr>
2020-07-10 15:57:03 +0200

    Add variables for URL

--- down.sh
+++ down.sh
@@ -23,8 +23,11 @@ read -p 'What is the server url (without "https" and "/" || e.g. google.com)? ==
 read -p 'What is the meeting ID? ==>' meetingID
 
 #download
-wget "https://$URL/presentation/$meetingID/video/webcams.mp4"
-wget "https://$URL/presentation/$meetingID/deskshare/deskshare.mp4"
+WEBCAMS="https://$URL/presentation/$meetingID/video/webcams.mp4"
+DESKSHARE="https://$URL/presentation/$meetingID/deskshare/deskshare.mp4"
+
+wget WEBCAMS
+wget DESKSHARE
 
 #Merge the recordings
 ffmpeg -i webcams.mp4 -i deskshare.mp4 -c copy MEETING-VIDEO.mp4
