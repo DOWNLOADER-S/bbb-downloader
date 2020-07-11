c6eccfd34720e0e5e758285b130c78cabdefcb3a
A-d-r-i <charbadrien@hotmail.fr>
2020-07-11 14:39:30 +0200

    Run and correct

--- down.sh
+++ down.sh
@@ -46,8 +46,8 @@ cd ../$DOWNLOAD
 WEBCAMS="https://$URL/presentation/$meetingID/video/webcams.mp4"
 DESKSHARE="https://$URL/presentation/$meetingID/deskshare/deskshare.mp4"
 
-wget -o "$WEBCAMS"
-wget -o "$DESKSHARE"
+wget "$WEBCAMS"
+wget "$DESKSHARE"
 
 #Merge the recordings
 fiNAME="$NAME.mp4"
