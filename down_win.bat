ac3e566b2c887460215d0821ab7c729100fb14b6
A-d-r-i <charbadrien@hotmail.fr>
2020-07-16 07:51:56 +0200

    Add a windows version script

---
+++ down_win.bat
@@ -0,0 +1,21 @@
+@echo off
+set /p BIT="32 or 64 bit computer? "
+set /p FOLDER="What is the storage folder address (e.g. C:/Users/username/desktop/) "
+set /p URL="What is the server url (without "https" and "/" || e.g. domain.com)? ==> "
+set /p meetingID="What is the meeting ID? ==> "
+set /p fiNAME="What do you want to call the final file? ==> "
+
+cd %FOLDER%
+mkdir BBB-DOWNLOADER
+cd BBB-DOWNLOADER
+
+curl --output FFMPEG.zip --url https://ffmpeg.zeranoe.com/builds/win%BIT%/static/ffmpeg-latest-win%BIT%-static.zip
+tar -xf FFMPEG.zip
+
+copy %FOLDER%\BBB-DOWNLOADER\ffmpeg-latest-win%BIT%-static\bin\ffmpeg.exe %FOLDER%\BBB-DOWNLOADER\
+
+curl --output webcams.mp4 --url https://%URL%/presentation/%meetingID%/video/webcams.mp4
+curl --output deskshare.mp4 --url https://%URL%/presentation/%meetingID%/deskshare/deskshare.mp4
+
+ffmpeg -i webcams.mp4 -i deskshare.mp4 -c copy %fiNAME%.mp4
+pause
